3fd277b19017fb10af88b35f590fee99593cacef
zero <moneymavie@gmail.com>
2025-04-19 05:24:42 -0400

    main mvp ui is pretty much finished

--- super-chinese-teacher/src/components/pages/ChatPage.js
+++ super-chinese-teacher/src/components/pages/ChatPage.js
@@ -1,8 +1,18 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ChatTemplate from '../templates/ChatTemplate';
+import OrbitalCharacterModels from '../molecules/OrbitalCharacterModels';
 import SEO from '../atoms/SEO';
 import { useChat } from '../../context/ChatContext';
+import './ChatPage.css';
+
+// Model data for background orbital models
+const characterModels = [
+  { src: '/models/ni.glb', character: '你' },
+  { src: '/models/hao.glb', character: '好' },
+  { src: '/models/wen.glb', character: '问' },
+  { src: '/models/zhong.glb', character: '中' }
+];
 
 const ChatPage = () => {
   const navigate = useNavigate();
@@ -20,13 +30,19 @@ const ChatPage = () => {
   };
 
   return (
-    <>
+    <div className="chat-page">
       <SEO 
         title="Chat with Your Teacher" 
         description="Have a conversation with your Chinese teacher and improve your language skills." 
       />
-      <ChatTemplate onBackClick={handleBackClick} />
-    </>
+      {/* Background Orbital Character Models */}
+      <OrbitalCharacterModels models={characterModels} centerX={50} centerY={50} />
+      
+      {/* Main Chat Component */}
+      <div className="chat-container">
+        <ChatTemplate onBackClick={handleBackClick} />
+      </div>
+    </div>
   );
 };
 
